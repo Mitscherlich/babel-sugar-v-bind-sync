0d2cf8537ab036552b5b4d99d672886f99aa9eb3
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-03-18 21:45:51 +0800

    test: setup vitest

---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+import { it, expect } from 'vitest'
+import { transformAsync } from '@babel/core';
+import transformJsx from '@vue/babel-plugin-transform-vue-jsx';
+import syntaxJSX from '@babel/plugin-syntax-jsx';
+import vBindSync from '..';
+
+const input = `function createApp() {
+  return (
+    <div>
+      <component visible_sync={this.test} foo="bar">This will be ok</component>
+      <component visible_sync={this.test[1]} foo="bar">This will be ok</component>
+      <component visible_sync={this.test.a} foo="bar">This will be ok</component>
+      <component visible_sync={this.test.a} open_sync={this.test.b}>This will be ok</component>
+      <component visible_sync={this.test} on={{click:this.test}}>This will be ok</component>
+    </div>
+  )
+}`
+
+it('should compiled match snapshot', async () => {
+  const output = await transformAsync(input, {
+    plugins: [syntaxJSX, vBindSync, transformJsx],
+  })
+
+  expect(output).matchSnapshot()
+})
